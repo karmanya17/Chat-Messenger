ee540f62b69ce925e3164867480efbf57b8fb370
karmanya17 <53150531+karmanya17@users.noreply.github.com>
2021-05-30 22:54:06 +0530

    Client-side Script

---
+++ public/script.js
@@ -0,0 +1,47 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined')
+  socket.emit('new-user', roomName, name)
+
+  messageForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    messageInput.value = ''
+  })
+}
+
+socket.on('room-created', function(room){
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
+
+socket.on('chat-message', function(data){
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', function(name){
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', function(name){
+  appendMessage(`${name} disconnected`)
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
